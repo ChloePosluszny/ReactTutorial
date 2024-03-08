bc76d85713cadfc6c7780d908f4e8c6f7a55d6d2
Chloë Posluszny <chloeposluszny@gmail.com>
2024-03-08 01:18:49 -0600

    Finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
+  let items = ["New York", "San Francisco", "Los Angeles", "Tokyo", "London"];
+  const handleSelectItem = (item) => {
+    console.log(item);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+      </div>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Cities"
+          onSelectItem={handleSelectItem}
+        />
+      </div>
+    </>
   );
 }
 
